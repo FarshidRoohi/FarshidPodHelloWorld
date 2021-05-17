2f02d2670c40d9f5dbcc53aee01e7e776f589a93
Farshidroohi <farshid.roohi.a@gmail.com>
2021-05-17 11:43:35 +0430

    init

---
+++ FarshidPodHelloWorld/FarshidPodHelloWorld.h
@@ -0,0 +1,18 @@
+//
+//  FarshidPodHelloWorld.h
+//  FarshidPodHelloWorld
+//
+//  Created by Farshid on 5/17/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FarshidPodHelloWorld.
+FOUNDATION_EXPORT double FarshidPodHelloWorldVersionNumber;
+
+//! Project version string for FarshidPodHelloWorld.
+FOUNDATION_EXPORT const unsigned char FarshidPodHelloWorldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FarshidPodHelloWorld/PublicHeader.h>
+
+
---
+++ FarshidPodHelloWorld/HellowWorld.swift
@@ -0,0 +1,15 @@
+//
+//  HellowWorld.swift
+//  FarshidPodHelloWorld
+//
+//  Created by Farshid on 5/17/21.
+//
+
+import Foundation
+
+public class HelloWorld {
+    
+    public static func say() -> String{
+        return "Hello, Human!"
+    }
+}
